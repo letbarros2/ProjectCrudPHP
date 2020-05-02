25a7abf80744aecea47782365428cbeb715a03b2
letbarros2 <leticiabarros99600512@gmail.com>
2020-05-02 13:37:05 -0300

    Função cadastrar, usando pdo e passando os parametros

--- CLASSES/usuarios.php
+++ CLASSES/usuarios.php
@@ -22,14 +22,42 @@ Class Usuario
 
     }
 
-    public function cadastar()
+    public function cadastar($nome,$email,$senha)
     {
         global $pdo;
-        global $msgErro;
+        //global $msgErro;
+        //passando tudo como parametro
+        //verificando se existe algum email cadastrado
+        //se vai retornar ou não um id
+        $sql = $pdo->prepare("SELECT id FROM usuarios WHERE email = :e");
+        $sql->bindValue(":e",$email);
+        $sql->execute();
+        //se existe algo na linha pq o row está contando as linhas
+        if($sql->rowCount()>0){
+
+            return false; //usuario cadastrado
+        }
+        else
+        {
+            // usuario não cadastrado, cadastrar
+            $sql = $pdo->prepare("INSERT INTO usuarios(nome,email,senha) VALUES (:n, e:, s:)");
+            $sql->bindValue(":n",$nome);
+            $sql->bindValue(":e",$email);
+            $sql->bindValue(":s",$senha);
+            $sql->execute();
+            return true; //cadastrado com sucesso.
+
+            
+
+
+        }
+        
+        //
+
 
     }
      
-    public function logar ()
+    public function logar ($email, $senha)
     {
         global $pdo;
         global $msgErro;
