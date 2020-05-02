c38fa1bca9c06ed40cd068d99608b56c45bb4500
letbarros2 <leticiabarros99600512@gmail.com>
2020-05-02 12:06:43 -0300

    Tela inicial, tela de cadastro junto com CSS e imagens

---
+++ cadastrar.php
@@ -0,0 +1,35 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title> Projeto Cunha</title>
+    <link rel = "stylesheet" href="CSS/estilo.css">
+
+
+</head>
+<body>
+    <div id="corpo-form-Cad">
+    
+    <h1> Cadastrar</h1>
+    
+    <form method="POST" action="processa.php">
+    <input type="text" placeholder="Nome completo">
+      
+
+    <input type="email" placeholder="Usuário">  
+    <input type = "password" placeholder="Senha">
+    <input type = "password" placeholder="Confirmar Senha"> 
+ 
+    <input type ="submit" value="Cadastrar">
+    
+    </form>
+    
+    </div>
+
+</body>
+
+
+
+
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title> Projeto Cunha</title>
+    <link rel = "stylesheet" href="CSS/estilo.css">
+
+
+</head>
+<body>
+    <div id="corpo-form">
+    
+    <h1> Entrar</h1>
+    
+    <form method="POST" action="processa.php">
+    
+    <input type="email" placeholder="Usuário">  
+    <input type = "password" placeholder="Senha"> 
+    <input type ="submit" value="Acessar">
+    <a href = "cadastrar.php">Ainda não é inscrito ?<strong>Cadastre-se</strong>
+    
+    </form>
+    
+    </div>
+
+</body>
+
+
+
+
+
+
+</html>
